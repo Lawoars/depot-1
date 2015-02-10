dad41037ea539761503b6dc596637dc9bac73d28
nikitasmall <nikitasosnov@yahoo.com>
2015-02-10 10:36:33 +0200

    update validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: {minimum: 5, message: 'Длина названия должна быть больше 5 символов!'}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: "Адрес должен оканчиваться на расширения png, jpg, gif"
