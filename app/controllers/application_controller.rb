4241302c9bb33c563a908afd0f8b99b8277bb918
nikitasmall <nikitasosnov@yahoo.com>
2015-02-10 22:19:39 +0200

    national curency, more tests, time adding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :getTime
+  
+  protected
+  def getTime 
+    @time = Time.now.to_s(:time)
+  end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,7 +6,9 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
     assert_select 'li a', minimum: 2
     assert_select 'div h3', minimum: 3
-    #assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select 'nav li', minimum: 2
+    assert_select 'h1', 'Каталог'
+    assert_select 'span h3', '9,00 грн.'
   end
 
 end
